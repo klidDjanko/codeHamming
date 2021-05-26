97237610d9e3895dd65b284bfd301047dc9475b6
klidDjanko <service.1.storage@gmail.com>
2021-05-26 09:21:20 +0300

    Доделал кодирование

---
+++ codeHamming/EnglishAlph.cs
@@ -0,0 +1,66 @@
+﻿namespace codeHamming
+{
+    class EnglishAlph
+    {
+        public string CharToBinary(string charEng)
+        {
+            string result = "";
+            switch(charEng)
+            {
+                case "a": result = "00001";
+                    break;
+                case "b": result = "00010";
+                    break;
+                case "c": result = "00011";
+                    break;
+                case "d": result = "00100";
+                    break;
+                case "e": result = "00101";
+                    break;
+                case "f": result = "00110";
+                    break;
+                case "g": result = "00111";
+                    break;
+                case "h": result = "01000";
+                    break;
+                case "i": result = "01001";
+                    break;
+                case "j": result = "01010";
+                    break;
+                case "k": result = "01011";
+                    break;
+                case "l": result = "01100";
+                    break;
+                case "m": result = "01101";
+                    break;
+                case "n": result = "01110";
+                    break;
+                case "o": result = "01111";
+                    break;
+                case "p": result = "10000";
+                    break;
+                case "q": result = "10001";
+                    break;
+                case "r": result = "10010";
+                    break;
+                case "s": result = "10011";
+                    break;
+                case "t": result = "10100";
+                    break;
+                case "u": result = "10101";
+                    break;
+                case "v": result = "10110";
+                    break;
+                case "w": result = "10111";
+                    break;
+                case "x": result = "11000";
+                    break;
+                case "y": result = "11001";
+                    break;
+                case "z": result = "11010";
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ codeHamming/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeHamming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Получаем ввод
+            Console.Write("Введите английский символ: ");
+            string input = Convert.ToString(Console.ReadLine());
+            //Получаем информационную часть в двоичном виде
+            EnglishAlph english = new EnglishAlph();
+            string binary = english.CharToBinary(input);
+            Console.WriteLine("Код символа в бинарном виде: {0}", binary);
+            //Кодируем, заполняем информационные биты кода Хемминга (9.4)
+            string[] codeHamming = new string[binary.Length + 4];
+            //Внешний цикл идёт по бинарному коду буквы
+            Program program = new Program();
+            for(int i = 0; i < binary.Length; i++)
+            {
+                //Внуренний цикл идёт по индексам кода Хемминга
+                for(int j = 0; j < codeHamming.Length; j++)
+                {
+                    //Если это не ключевой блок с проверочным битом и если он не занят, запишем информационный бит
+                    if (Math.Log(j + 1, 2) % 1 != 0 && codeHamming[j] == null)
+                    {
+                        codeHamming[j] = binary[i].ToString();
+                        break;
+                    }
+                    else if(Math.Log(j + 1, 2) % 1 == 0)
+                    {
+                        if (j + 1 == 1) codeHamming[j] = program.Xor(binary[1].ToString(), binary[2].ToString());
+                        if (j + 1 == 2) codeHamming[j] = program.Xor(program.Xor(binary[0].ToString(), binary[2].ToString()), binary[3].ToString());
+                        if (j + 1 == 4) codeHamming[j] = program.Xor(program.Xor(binary[1].ToString(), binary[3].ToString()), binary[4].ToString());
+                        if (j + 1 == 8) codeHamming[j] = program.Xor(program.Xor(binary[0].ToString(), binary[1].ToString()), binary[4].ToString());
+                    }
+                }
+            }
+
+            //Вывод кода на консоль
+            Console.Write("Полученный код Хемминга: ");
+            foreach (string el in codeHamming) Console.Write(el);
+            Console.ReadLine();
+        }
+
+        string Xor(string a, string b)
+        {
+            string result = "0";
+            if (a == "0" && b == "0" || a == "1" && b == "1") result = "0";
+            else result = "1";
+            return result;
+        }
+    }
+}
